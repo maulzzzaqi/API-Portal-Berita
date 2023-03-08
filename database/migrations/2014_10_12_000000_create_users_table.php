125a492ce874245ada66766e64c3343a654e1bb0
MaulzzZaqi <madezaqi2006@gmail.com>
2023-03-08 13:35:50 +0700

    testing get api

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('password');
             $table->string('firstname');
             $table->string('lastname')->nullable();
-            $table->rememberToken();
             $table->timestamps();
 
             $table->softDeletes();
