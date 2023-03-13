00f16dd9b4fad62bc164c1ba6e17bd75681c1e79
MaulzzZaqi <madezaqi2006@gmail.com>
2023-03-13 09:26:39 +0700

    custom api with detailed post resources

--- app/Http/Resources/PostDetailResource.php
+++ app/Http/Resources/PostDetailResource.php
@@ -4,8 +4,9 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use NunoMaduro\Collision\Writer;
 
-class PostResource extends JsonResource
+class PostDetailResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -19,6 +20,8 @@ class PostResource extends JsonResource
             'id' => $this->id,
             'title' => $this->title,
             'news_content' => $this->news_content,
+            // 'author' => $this->author,
+            'writer' => $this->writer,
             'created_at' => date_format($this->created_at, "m/d/y h:i:s"),
         ];
     }
