bf32d1914ec359205ea050743c7d9c0203cc88fb
MaulzzZaqi <madezaqi2006@gmail.com>
2023-03-15 13:24:00 +0700

    create post

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/posts', [PostController::class, 'index'])->middleware(['auth:sanctum']);
-Route::get('/posts/{id}', [PostController::class, 'show'])->middleware(['auth:sanctum']);
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::get('/posts', [PostController::class, 'index'])->middleware(['auth:sanctum']);
+    Route::get('/posts/{id}', [PostController::class, 'show'])->middleware(['auth:sanctum']);
+    Route::post('/posts', [PostController::class, 'store']);
+
+    Route::get('/logout', [AuthenticationController::class, 'logout'])->middleware('auth:sanctum');
+    Route::get('/account', [AuthenticationController::class, 'account'])->middleware('auth:sanctum');
+});
+
 Route::get('/posts2/{id}', [PostController::class, 'show2']);
 Route::post('/login', [AuthenticationController::class, 'login']);
-Route::get('/logout', [AuthenticationController::class, 'logout'])->middleware('auth:sanctum');
-Route::get('/account', [AuthenticationController::class, 'account'])->middleware('auth:sanctum');
